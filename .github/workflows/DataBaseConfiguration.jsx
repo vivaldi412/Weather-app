44465ae26b6178b1ff0b13c50ef0b05cfe4ccde4
vivaldi412 <152299713+vivaldi412@users.noreply.github.com>
2024-10-02 19:32:31 +0330

    Update DataBaseConfiguration.jsx

--- .github/workflows/DataBaseConfiguration.jsx
+++ .github/workflows/DataBaseConfiguration.jsx
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vzcxbqqlcnknhdkzxkmm.supabase.co'
-const supabaseKey = ${{ secrets.SUPA_BASE_KEY }}
+const supabaseKey = SUPA_BASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
