03914997681505a6c16c4c01e17c955be6cdf3e4
vivaldi412 <152299713+vivaldi412@users.noreply.github.com>
2024-10-02 14:13:13 +0330

    Delete src/DataBaseConfiguration.jsx

--- src/DataBaseConfiguration.jsx
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vzcxbqqlcnknhdkzxkmm.supabase.co'
-const supabaseKey = ${{ secrets.SUPA_BASE_KEY }}
-export const supabase = createClient(supabaseUrl, supabaseKey)
